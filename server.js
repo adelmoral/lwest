879b19b13427329f87798cfa8c3c7ef07e413b7a
adelmoral <adelmoral13@gmail.com>
2017-01-04 19:46:50 -0600

    First round of information additions and aesthetics.

--- server.js
+++ server.js
@@ -15,6 +15,37 @@ app.get('/', function (req, res, next) {
   }
 })
 
+app.set('views', './source/templates')
+app.set('view engine', 'pug')
+
+app.get('/:path', function (req, res, next) {
+  try {
+    switch (req.params.path) {
+      case 'home':
+        res.render('homepage.jade')
+        break;
+      case 'story':
+        res.render('story.jade')
+        break;
+      case 'wedding':
+        res.render('wedding.jade')
+        break;
+	  case 'accommodations':
+        res.render('accommodations.jade')
+        break;
+	  case 'registry':
+        res.render('registry.jade')
+        break;
+      default:
+		res.render('homepage.jade')
+    }
+
+
+  } catch (e) {
+    next(e)
+  }
+})
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
 })
